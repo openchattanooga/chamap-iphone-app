a1b193923d04614a7349f71a20de217d3dc21da6
Tim Moreland <aplannersguide@gmail.com>
2016-04-30 23:03:37 -0400

    MVP iOS App
    Followed these instructions
    (https://www.mapbox.com/help/first-steps-ios-sdk/) to get the basic app
    up and running.

---
+++ ChaMap/ChaMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChaMap
+//
+//  Created by Moreland Tim on 4/30/16.
+//  Copyright © 2016 Open Chattanooga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChaMap/ChaMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChaMap
+//
+//  Created by Moreland Tim on 4/30/16.
+//  Copyright © 2016 Open Chattanooga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
